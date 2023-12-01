ccdf26d4560c7cb5aea61a07a03d2416e1f05313
monimax222 <151806853+monimax222@users.noreply.github.com>
2023-12-01 10:33:44 +0500

    Add files via upload

---
+++ alle.js
@@ -0,0 +1,14 @@
+const dateControl = document.querySelector('input[type="date"]');
+dateControl.value = "2017-06-01";
+console.log(dateControl.value); 
+console.log(dateControl.valueAsNumber);
+
+                const dateControl = document.querySelector('input[type="date"]');
+dateControl.value = "2017-06-01";
+console.log(dateControl.value); 
+console.log(dateControl.valueAsNumber);
+
+const dateControl = document.querySelector('input[type="date"]');
+dateControl.value = "2017-06-01";
+console.log(dateControl.value); 
+console.log(dateControl.valueAsNumber);
